17158f19de241df0672c383d58b42530e34fb46d
Nao YONASHIRO <owan.orisano@gmail.com>
2018-09-04 23:48:14 +0900

    chore: remove t flag

--- rget.sh
+++ rget.sh
@@ -17,7 +17,7 @@ shift $(($OPTIND - 1))
 WORKDIR=`mktemp -d`
 
 CONTENT_LENGTH=`wget --spider -S ${1} 2>&1 | grep Content-Length | awk '$0=$2'`
-seq 0 $BLOCKSIZE $(($CONTENT_LENGTH - 1)) | awk -v BS=$BLOCKSIZE -v WD=$WORKDIR '{f=sprintf("%s/%05d",WD,NR);printf("echo > %s && wget -q -c --header \"Range: bytes=%d-%d\" -O %s\n", f, $0, $0 + BS - 1, f)}' | xargs -t -n 12 -P ${PROCESS} -I{} sh -c "{} $1"
+seq 0 $BLOCKSIZE $(($CONTENT_LENGTH - 1)) | awk -v BS=$BLOCKSIZE -v WD=$WORKDIR '{f=sprintf("%s/%05d",WD,NR);printf("echo > %s && wget -q -c --header \"Range: bytes=%d-%d\" -O %s\n", f, $0, $0 + BS - 1, f)}' | xargs -n 12 -P ${PROCESS} -I{} sh -c "{} $1"
 for f in $WORKDIR/*; do
     tail -c +2 $f >> $OUTPUT
 done
