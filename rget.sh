c4cb1f839f167a19151e4d01549bf338f849b1ac
Nao YONASHIRO <owan.orisano@gmail.com>
2018-09-04 23:47:02 +0900

    feat: remove dependency(curl)

--- rget.sh
+++ rget.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 set -e
-WORKDIR=`mktemp -d`
 
 PROCESS=4
 BLOCKSIZE=8388608
-while getopts "P:b:o:u:" OPTION
+while getopts "P:b:o:" OPTION
 do
   case $OPTION in
     P) PROCESS=${OPTARG};;
     b) BLOCKSIZE=${OPTARG};;
     o) OUTPUT=${OPTARG};;
-    *) exit $E_OPTERROR;;
+    *) exit 2;;
   esac
 done
 shift $(($OPTIND - 1))
 
+WORKDIR=`mktemp -d`
+
 CONTENT_LENGTH=`wget --spider -S ${1} 2>&1 | grep Content-Length | awk '$0=$2'`
-seq 0 $BLOCKSIZE $CONTENT_LENGTH | awk -v BS=$BLOCKSIZE -v WD=$WORKDIR '{printf("-SsL -r %d-%d -o %s/%05d\n", $0, $0 + BS - 1, WD, NR)}' | xargs -n 5 -P $PROCESS -I{} sh -c "curl {} $1"
-cat $WORKDIR/* > $OUTPUT
+seq 0 $BLOCKSIZE $(($CONTENT_LENGTH - 1)) | awk -v BS=$BLOCKSIZE -v WD=$WORKDIR '{f=sprintf("%s/%05d",WD,NR);printf("echo > %s && wget -q -c --header \"Range: bytes=%d-%d\" -O %s\n", f, $0, $0 + BS - 1, f)}' | xargs -t -n 12 -P ${PROCESS} -I{} sh -c "{} $1"
+for f in $WORKDIR/*; do
+    tail -c +2 $f >> $OUTPUT
+done
 rm -rf $WORKDIR
